d5f9c9174c4f428ee25791750c9fcdb2e77671e7
Liubzzz <cosmin.liuba67@gmail.com>
2023-05-05 15:15:45 +0300

    succesfully brought data from node server to frontend

--- backend101/backendToFrontData/backEndToFrontEndData.js
+++ backend101/backendToFrontData/backEndToFrontEndData.js
@@ -1,29 +1,14 @@
-// const express = require("express");
-// const fs = require("fs");
-
-// const roomName = express();
-// const port = 3001;
-
-// roomName.get("/meetingRoomData", (req, res) => {
-//   fs.readFile("./meetingRoomData.json", "utf8", (err, data) => {
-//     if (err) throw err;
-//     res.json(JSON.parse(data));
-//   });
-// });
-
-// roomName.listen(port, () => {
-//   console.log(`Server listening at http://localhost:${port}`);
-// });
-
 const express = require("express");
-const roomName= express();
-const port = 3001;
 
-roomName.get("/meetingRoomData", (req, res) => {
+const bookRoomApp = express();
+var cors = require("cors");
+
+bookRoomApp.use(cors());
+
+bookRoomApp.get("/meetingRoomData", (req, res) => {
   const data = require("./meetingRoomData.json");
   res.json(data.roomdata);
 });
-
-roomName.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+bookRoomApp.listen(3001, () =>
+  console.log(`Server running at http://localhost:3001`)
+);
